d54d994478e5ecf05a77468077433eadfcaa5096
ScurraMagna <55872534+ScurraMagna@users.noreply.github.com>
2020-12-30 14:44:35 +0000

    Update glow.js

--- glow.js
+++ glow.js
@@ -2,6 +2,6 @@ let letters = document.getElementsByTagName('h1')[0];
 let n = letters.children.length; // number of letter
 let d = 4; // animation duration
 
-for (let i=0; i<letters.children.length; i++) {
+for (let i=0; i<n; i++) {
   letters.children[i].style.animationDelay = (i / 2 * d / n) + "s";
 }
